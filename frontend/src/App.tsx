64a6c227cff9481b4ce30ac857044d5942f0307e
BECATRUE <becatrueyjh@gmail.com>
2022-10-27 13:34:59 +0900

    feat: Update App.tsx
    Add create custom page routing.
    
    See also: 20

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import InitPage from './InitPage/InitPage';
 import ItemDetailPage from './ItemDetailPage/ItemDetailPage';
+import CreateCustomPage from './CreateCustomPage/CreateCustomPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<InitPage />} />
+          <Route path='/custom/create' element={<CreateCustomPage />} />
           <Route path="/:type/:id" element={<ItemDetailPage />} />
         </Routes>
       </BrowserRouter >
