75f7ac1f45ec6db799e9a6c021164b5d62cac7c5
AlongwithKiman <expfunsce@snu.ac.kr>
2022-11-03 02:41:51 +0900

    feat: ingredient_prepare GET POST

--- backend/qualla/ingredient_prepare/models.py
+++ backend/qualla/ingredient_prepare/models.py
@@ -7,12 +7,19 @@ from ingredient.models import Ingredient
 
 class IngredientPrepare(models.Model):
     cocktail = models.ForeignKey(
-        Cocktail, on_delete=models.CASCADE, related_name='ingredients', null=True)
+        Cocktail, on_delete=models.CASCADE, related_name='ingredient_prepare', null=False)
     ingredient = models.ForeignKey(
-        Ingredient, on_delete=models.PROTECT, related_name='ingredients', null=False)  # 재료가 Null이 될 수 없다.
+        Ingredient, on_delete=models.PROTECT, related_name='ingredient_prepare', null=False)
 
     # amount : `${숫자} {단위}`, ex) 1 oz, 60 ml, 1 개, 1 꼬집
     # 추후 도수 계산 시 oz, ml등의 단위에 대해서는 정량적 계산, 그 외의 경우에는 부피가 없음 가정
     AMOUNT_REGEX = RegexValidator(r'(\d*\.?\d+)\s[a-z|A-Z|ㄱ-ㅎ|가-힣]+')
     amount = models.CharField(
         max_length=50, null=False, validators=[AMOUNT_REGEX])
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['cocktail', 'ingredient'], name='unique_cocktail_ingredient_combination'
+            )
+        ]
--- backend/qualla/ingredient_prepare/views.py
+++ backend/qualla/ingredient_prepare/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from functools import partial
+from django.http import HttpResponseBadRequest, HttpResponseNotAllowed, HttpResponseNotFound, JsonResponse
+from rest_framework.decorators import api_view
+from cocktail.models import Cocktail
+from .serializers import IngredientPrepareSerializer
+from django.db import IntegrityError
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def ingredient_list(request, cocktail_id):
+    if request.method == 'GET':
+        try:
+            cocktail = Cocktail.objects.get(id=cocktail_id)
+        except Cocktail.DoesNotExist:
+            return HttpResponseNotFound(f"No Cocktail matches id={cocktail_id}")
+
+        ingredient_prepare = cocktail.ingredient_prepare.all()
+        # id, name, image, ABV, price, amount
+        return_data = [{"id": element.ingredient.id, "name": element.ingredient.name, "image": element.ingredient.image,
+                       "ABV": element.ingredient.ABV, "price": element.ingredient.price, "amount": element.amount}
+                       for element in ingredient_prepare]
+
+        return JsonResponse(return_data, safe=False)
+    elif request.method == 'POST':
+        try:
+            cocktail = Cocktail.objects.get(id=cocktail_id)
+        except Cocktail.DoesNotExist:
+            return HttpResponseNotFound(f"No Cocktail matches id={cocktail_id}")
+
+        try:
+            data = request.data.copy()
+            # TODO: change fields that is derived automatically
+            data['cocktail'] = cocktail_id
+            serializer = IngredientPrepareSerializer(
+                data=data, context={"request": request})
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+        except (IntegrityError) as e:
+            return HttpResponseBadRequest()
+        return JsonResponse(serializer.data, status=201)
+    else:
+        return HttpResponseNotAllowed(['GET', 'POST'])
