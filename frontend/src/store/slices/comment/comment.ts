fb6f691237a507ff9bb46ed6d7480ddfde874fec
nemga123 <hspark0419@naver.com>
2022-11-03 01:09:10 +0900

    feat: Feature Comment Store - feature comment store with dummy data - connect backend when comment model is implemented

--- frontend/src/store/slices/comment/comment.ts
+++ frontend/src/store/slices/comment/comment.ts
@@ -1,5 +1,6 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit"
 import axios from "axios"
+import { RootState } from "../.."
 
 export interface CommentType {
     id: number,
@@ -48,8 +49,8 @@ const initialState: CommentInfo = {
 }
 
 export const fetchCommentListByCocktailId = createAsyncThunk(
-    "comment/fetchCommentListByCocktailId", async (id) => {
-        const response = await axios.get(`/api/v1/comment/cocktails/${id}/`)
+    "comment/fetchCommentListByCocktailId", async (cocktail_id) => {
+        const response = await axios.get(`/api/v1/comment/cocktails/${cocktail_id}/`)
         return response.data
     }
 )
@@ -61,14 +62,81 @@ export const fetchMyCommentList = createAsyncThunk(
     }
 )
 
+export const getComment = createAsyncThunk(
+    "comment/getComment", async (id) => {
+        const response = await axios.get(`/api/v1/comment/${id}/`)
+        return response.data
+    }
+)
+
+export const postComment = createAsyncThunk(
+    "comment/postComment", async (comment: Pick<CommentType, "content"|"parent"|"cocktail_id">, {dispatch}) => {
+        const response = await axios.post(`/api/v1/comment/cocktails/${comment.cocktail_id}/`, {
+            "content": comment.content,
+            "parent": comment.parent
+        })
+        dispatch(commentActions.addComment(response.data))
+    }
+)
+
+export const editComment = createAsyncThunk(
+    "comment/editComment", async (comment: Pick<CommentType, "content"|"id">, {dispatch}) => {
+        const response = await axios.put(`/api/v1/comment/${comment.id}/`, {
+            "content": comment.content,
+        })
+        dispatch(commentActions.editComment(response.data))
+    }
+)
+
+export const deleteComment = createAsyncThunk(
+    "comment/deleteComment", async (id: number, {dispatch}) => {
+        const response = await axios.put(`/api/v1/comment/${id}/`)
+        dispatch(commentActions.deleteComment(id))
+    }
+)
+
 export const CommentSlice = createSlice({
     name: "comment",
     initialState,
     reducers: {
+        addComment: (state, action: PayloadAction<CommentType>) => {
+            state.commentList.push(action.payload)
+        },
+        editComment: (state, action: PayloadAction<CommentType>) => {
+            state.commentList.map((comment) => {
+                return comment.id == action.payload.id ? action.payload : comment
+            });
+        },
+        deleteComment: (state, action) => {
+            const deleted = state.commentList.filter((comment) => {
+                return comment.id != action.payload.id;
+            });
+            state.commentList = deleted
+        }
     },
     extraReducers: (builder) => {
         builder.addCase(fetchCommentListByCocktailId.fulfilled, (state, action) => {
             state.commentList = action.payload
-        })
+        });
+        builder.addCase(fetchMyCommentList.fulfilled, (state, action) => {
+            state.commentList = action.payload
+        });
+        builder.addCase(getComment.fulfilled, (state, action) => {
+            state.commentItem = action.payload
+        });
+        builder.addCase(postComment.rejected, (_state, action) => {
+            console.error(action.error)
+        });
+        builder.addCase(editComment.rejected, (_state, action) => {
+            console.error(action.error)
+        });
+        builder.addCase(deleteComment.rejected, (_state, action) => {
+            console.error(action.error)
+        });
     }
 })
+
+export const commentActions = CommentSlice.actions
+export const selectComment = (state: RootState) => state.cocktail
+
+export default CommentSlice.reducer
