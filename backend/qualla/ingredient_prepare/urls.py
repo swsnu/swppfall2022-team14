16b5fdda0316372e6188a2ffe95efe1950fb9a3b
AlongwithKiman <expfunsce@snu.ac.kr>
2022-11-03 18:51:29 +0900

    PUT, DELETE ingredient_prepare with object id

--- backend/qualla/ingredient_prepare/urls.py
+++ backend/qualla/ingredient_prepare/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('<int:cocktail_id>/ingredients/', views.ingredient_list,
-         name='need ingredient list')
+         name='need ingredient list'),
+    path('<int:cocktail_id>/ingredients/<int:ingredient_id>/',
+         views.ingredient_prepare_modify, name='cocktail ingredient modify')
 ]
