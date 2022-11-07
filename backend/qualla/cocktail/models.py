ecb235d7db95f657476e5992abed3938ae99fc6c
nemga123 <hspark0419@naver.com>
2022-11-07 15:05:07 +0900

    feat : Add Rate field in cocktail model - cocktail model에서 rate를 랜덤으로 리턴하지 않고 필드로 가지고 있음 - 이후 rate 설정하는 feature구현 필요

--- backend/qualla/cocktail/models.py
+++ backend/qualla/cocktail/models.py
@@ -18,6 +18,7 @@ class Cocktail(models.Model):
     author_id = models.IntegerField(null=True, default=None)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    rate = models.FloatField(default=0.0, null=False)
 
     class Meta:
         constraints = [
