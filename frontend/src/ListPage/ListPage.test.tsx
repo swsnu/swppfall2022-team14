1f8f60bc09a58cf242ada12c237484d1c3e8ce8c
nemga123 <hspark0419@naver.com>
2022-11-10 03:19:09 +0900

    test: Testcode foe ListPage.tsx

--- frontend/src/ListPage/ListPage.test.tsx
+++ frontend/src/ListPage/ListPage.test.tsx
@@ -1,11 +1,31 @@
 import { Provider } from "react-redux"
 import { CocktailDetailType, CocktailInfo, CocktailItemType } from "../store/slices/cocktail/cocktail"
 import { CommentInfo } from "../store/slices/comment/comment"
-import { IngredientInfo } from "../store/slices/ingredient/ingredient"
+import { IngredientInfo, IngredientType } from "../store/slices/ingredient/ingredient"
 import { getMockStore } from "../test-utils/mock"
+import { MemoryRouter, Navigate, Route, Routes } from "react-router";
+
 import ListPage from "./ListPage"
 import React from 'react';
-import { render } from "@testing-library/react"
+
+import { render, screen, waitFor } from "@testing-library/react"
+import axios from "axios"
+
+jest.mock("./Item/Item", () => (prop:Pick<CocktailItemType, "image" | "name" | "rate" | "type" | "id" | "tags">) => (
+    <div data-testid={`spyComment_${prop.id}`}>
+    </div>
+));
+
+jest.mock("./Ingr/Ingr", () => (prop:Pick<IngredientType, "image" | "name" | "id">) => (
+    <div data-testid={`spyComment_${prop.id}`}>
+    </div>
+));
+
+jest.mock("../NavBar/NavBar", () => () => (
+    <div data-testid={`spyComment_nav_bar`}>
+    </div>
+));
+
 const standard_cocktail1_item: CocktailItemType = {
     id: 1,
     name: "ST_COCKTAIL1",
@@ -16,19 +36,8 @@ const standard_cocktail1_item: CocktailItemType = {
     rate: 0
 }
 
-const standard_cocktail1_detail: CocktailDetailType = {
-    ...standard_cocktail1_item,
-    introduction: "INTRO1",
-    recipe: "RECIPE1",
-    ABV: 10,
-    price_per_glass: 80000,
-    created_at: new Date(Date.now()),
-    updated_at: new Date(Date.now()),
-    ingredients: []
-}
-
 const custom_cocktail1_item: CocktailItemType = {
-    id: 1,
+    id: 2,
     name: "CS_COCKTAIL1",
     image: "IMAGE1",
     type: "CS",
@@ -37,19 +46,15 @@ const custom_cocktail1_item: CocktailItemType = {
     rate: 0
 }
 
-const custom_cocktail1_detail: CocktailDetailType = {
-    ...custom_cocktail1_item,
-    introduction: "INTRO2",
-    recipe: "RECIPE2",
-    ABV: 10,
-    price_per_glass: 80000,
-    created_at: new Date(Date.now()),
-    updated_at: new Date(Date.now()),
-    ingredients: []
+const stubInitialStandardCocktaiState: CocktailInfo = {
+    cocktailList: [standard_cocktail1_item],
+    cocktailItem: null,
+    itemStatus: "success",
+    listStatus: "success",
 }
 
-const stubInitialCocktaiState: CocktailInfo = {
-    cocktailList: [ standard_cocktail1_item, custom_cocktail1_item ],
+const stubInitialCustomCocktaiState: CocktailInfo = {
+    cocktailList: [custom_cocktail1_item],
     cocktailItem: null,
     itemStatus: "success",
     listStatus: "success",
@@ -60,30 +65,120 @@ const emptyCommentState: CommentInfo = {
     commentItem: null,
     state: null,
 }
+const ingredient: IngredientType = {
+    id: 1,
+    name: "INGREDIENT1",
+    image: "IMAGE1",
+    ABV: 0,
+    price: 0,
+    introduction: "INTRODUCTION1",
+}
 
-const emptyIngredientState: IngredientInfo = {
-    ingredientList: [],
+const ingredientState: IngredientInfo = {
+    ingredientList: [ingredient],
     ingredientItem: null,
     itemStatus: "success",
     listStatus: "success",
 }
 
-jest.mock("react-router-dom", () => ({
-    ...jest.requireActual("react-router-dom")
-}))
-const mockStore = getMockStore({cocktail: stubInitialCocktaiState, ingredient: emptyIngredientState, comment: emptyCommentState})
+let standardMockStore = getMockStore({cocktail: stubInitialStandardCocktaiState, ingredient: ingredientState, comment: emptyCommentState});
 
-const listPage: JSX.Element = ( 
-    <Provider store={mockStore}>
-        <ListPage/>
-    </Provider>
-)
+const customMockStore = getMockStore({cocktail: stubInitialCustomCocktaiState, ingredient: ingredientState, comment: emptyCommentState});
+
+jest.mock("react-redux", () => ({
+    ...jest.requireActual("react-redux"),
+    useDispatch: () => jest.fn()
+}));
+
+jest.mock("react-router-dom", () => ({
+    ...jest.requireActual("react-router-dom"),
+    useLocation: () => jest.fn()
+}));
+
 describe("<ListPage />", () => {
     beforeEach(() => {
         jest.clearAllMocks();
-    })
-    it("should render without errors", () => {
-        const { container } = render(listPage);
+    });
+
+    it("should render standard cocktail without errors", () => {
+        render(    
+            <Provider store={standardMockStore}>
+                <MemoryRouter initialEntries={['/standard']}>
+                    <Routes>
+                        <Route path="/:type" element={<ListPage/>}/>
+                    </Routes>
+                </MemoryRouter>
+            </Provider>
+        ); 
+        const items = screen.getAllByTestId("spyComment_1");
+        expect(items).toHaveLength(1);
+    });
+
+    it("should render standard cocktail without errors", () => {
+        render(
+            <Provider store={customMockStore}>
+                <MemoryRouter initialEntries={['/custom']}>
+                    <Routes>
+                        <Route path="/:type" element={<ListPage/>}/>
+                    </Routes>
+                </MemoryRouter>
+            </Provider>
+        );
+        const items = screen.getAllByTestId("spyComment_2");
+        expect(items).toHaveLength(1);
+    });
+
+    it("should render ingredients without errors", () => {
+        render(
+            <Provider store={customMockStore}>
+                <MemoryRouter initialEntries={['/ingredient']}>
+                    <Routes>
+                        <Route path="/:type" element={<ListPage/>}/>
+                    </Routes>
+                </MemoryRouter>
+            </Provider>
+        );
+        const items = screen.getAllByTestId("spyComment_1");
+        expect(items).toHaveLength(1);
+    });
+
+    it("should render list when pending axios without errors", () => {
+        const stubCocktaiState: CocktailInfo = {
+            cocktailList: [standard_cocktail1_item],
+            cocktailItem: null,
+            itemStatus: "success",
+            listStatus: "loading",
+        };
+        const mockStore = getMockStore({cocktail: stubCocktaiState, ingredient: ingredientState, comment: emptyCommentState});
+        const {container} = render(    
+            <Provider store={mockStore}>
+                <MemoryRouter initialEntries={['/standard']}>
+                    <Routes>
+                        <Route path="/:type" element={<ListPage/>}/>
+                    </Routes>
+                </MemoryRouter>
+            </Provider>
+        ); 
         expect(container).toBeTruthy();
-    })
+    });
+
+    it("should render list when failed axios without errors", () => {
+        const stubCocktaiState: CocktailInfo = {
+            cocktailList: [standard_cocktail1_item],
+            cocktailItem: null,
+            itemStatus: "success",
+            listStatus: "failed",
+        };
+        const mockStore = getMockStore({cocktail: stubCocktaiState, ingredient: ingredientState, comment: emptyCommentState});
+        const {container} = render(    
+            <Provider store={mockStore}>
+                <MemoryRouter initialEntries={['/standard']}>
+                    <Routes>
+                        <Route path="/:type" element={<ListPage/>}/>
+                    </Routes>
+                </MemoryRouter>
+            </Provider>
+        ); 
+        expect(container).toBeTruthy();
+    });
 })
