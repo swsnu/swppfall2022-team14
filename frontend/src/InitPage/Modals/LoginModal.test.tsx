dd55c45d622571b4fa88c12338a5664fd0080887
BECATRUE <becatrueyjh@gmail.com>
2022-11-12 01:03:01 +0900

    feat: Test render LoginModal

---
+++ frontend/src/InitPage/Modals/LoginModal.test.tsx
@@ -0,0 +1,67 @@
+import { MemoryRouter, Route, Routes } from "react-router";
+import { screen, fireEvent } from "@testing-library/react";
+import { renderWithProviders } from "../../test-utils/mock";
+import { CocktailInfo } from "../../store/slices/cocktail/cocktail";
+import { CommentInfo } from "../../store/slices/comment/comment";
+import { IngredientInfo } from "../../store/slices/ingredient/ingredient";
+import LoginModal from "./LoginModal";
+
+const stubCocktailInitialState: CocktailInfo = {
+    cocktailList: [],
+    cocktailItem: null,
+    itemStatus: "loading",
+    listStatus: "loading",
+}
+
+const stubCommentInitialState: CommentInfo = {
+    commentList: [],
+    commentItem: null,
+    state: null,
+};
+
+const stubIngredientInitialState: IngredientInfo = {
+    ingredientList: [],
+    ingredientItem: null,
+    itemStatus: "loading",
+    listStatus: "loading",
+};
+
+jest.mock("react-modal", () => (props: {className: any, isOpen: boolean, onRequestClose: any, children: React.ReactNode}) => {
+    props.onRequestClose()
+    if (props.isOpen) return (
+        <div data-testid={"spyModal_opened"}>
+            {props.children}
+        </div>
+    )
+    else return <div data-testid={"spyModal_closed"} />
+});
+
+const mockDispatch = jest.fn();
+jest.mock("react-redux", () => ({
+    ...jest.requireActual("react-redux"),
+    useDispatch: () => mockDispatch,
+}));
+
+const renderLoginModal = () => {
+    renderWithProviders(
+        <MemoryRouter>
+            <Routes>
+                <Route path="/" element={<LoginModal isOpen={true} setIsOpen={ jest.fn() } setLoginState={ jest.fn() } />} />
+            </Routes>
+        </MemoryRouter>,
+        {
+            preloadedState: {
+                cocktail: stubCocktailInitialState,
+                comment: stubCommentInitialState,
+                ingredient: stubIngredientInitialState,
+            },
+        }
+    );
+};
+
+describe("<LoginModal />", () => {
+    it("should render LoginModal", async () => {
+        renderLoginModal();
+        await screen.findByText("Login");
+    });
+});
