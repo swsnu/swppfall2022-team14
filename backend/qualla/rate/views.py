2475b7a14ed55144c0a03648560ed0936a35e632
BECATRUE <becatrueyjh@gmail.com>
2022-11-13 02:19:48 +0900

    feat: Implement get rate list
    Get rate list according to specific cocktail.
    Add score to rate model.
    
    See also: #101

--- backend/qualla/rate/views.py
+++ backend/qualla/rate/views.py
@@ -1,13 +1,20 @@
-from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse
-import json
+from django.http import HttpResponseNotAllowed, HttpResponseNotFound, JsonResponse
 from rest_framework.decorators import api_view
-from .models import Rate
+from cocktail.models import Cocktail
 
 
 @api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def rate_list(request, cocktail_id):
     if request.method == 'GET':
-        pass
+        try:
+            cocktail = Cocktail.objects.get(id=cocktail_id)
+        except Cocktail.DoesNotExist:
+            return HttpResponseNotFound(f"No Cocktail matches id={cocktail_id}")
+
+        rate_list = cocktail.rate_set.all()
+        data = [{"cocktail_id": cocktail_id, "user_id": rate.user_id, "score": rate.score}
+                for rate in rate_list]
+        return JsonResponse(data, safe=False)
     elif request.method == 'POST':
         pass
     elif request.method == 'PUT':
