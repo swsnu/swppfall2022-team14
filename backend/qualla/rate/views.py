3f901127069719c74f1d6d9f8a4006276d189e64
BECATRUE <becatrueyjh@gmail.com>
2022-11-13 02:07:37 +0900

    feat: Initailize views
    See also: #101

--- backend/qualla/rate/views.py
+++ backend/qualla/rate/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse
+import json
+from rest_framework.decorators import api_view
+from .models import Rate
 
-# Create your views here.
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def rate_list(request, cocktail_id):
+    if request.method == 'GET':
+        pass
+    elif request.method == 'POST':
+        pass
+    elif request.method == 'PUT':
+        pass
+    elif request.method == 'DELETE':
+        pass
+    else:
+        return HttpResponseNotAllowed(['GET', 'POST', 'PUT', 'DELETE'])
