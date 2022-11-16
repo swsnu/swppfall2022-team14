21431da33998ab0ee43b07ad848302fc6ebbe70e
BECATRUE <becatrueyjh@gmail.com>
2022-11-16 10:22:24 +0900

    feat: Add AddIngredientModal test

---
+++ frontend/src/CreateCustomPage/Modals/AddIngredientModal.test.tsx
@@ -0,0 +1,74 @@
+import { MemoryRouter, Route, Routes } from "react-router";
+import { screen, fireEvent } from "@testing-library/react";
+import { renderWithProviders } from "../../test-utils/mock";
+import { CocktailInfo } from "../../store/slices/cocktail/cocktail";
+import { CommentInfo } from "../../store/slices/comment/comment";
+import { IngredientInfo } from "../../store/slices/ingredient/ingredient";
+import AddIngredientModal from "./AddIngredientModal";
+
+const stubCocktailInitialState: CocktailInfo = {
+    cocktailList: [],
+    cocktailItem: null,
+    itemStatus: "loading",
+    listStatus: "loading",
+}
+
+const stubCommentInitialState: CommentInfo = {
+    commentList: [],
+    commentItem: null,
+    state: null,
+};
+
+const stubIngredientInitialState: IngredientInfo = {
+    ingredientList: [
+        {
+            id: 1,
+            name: 'INGREDIENT_NAME_1',
+            image: 'INGREDIENT_IMAGE_1',
+            introduction: 'INGREDIENT_INTRO_1',
+            ABV: 40,
+            price: 200
+        },
+        {
+            id: 2,
+            name: 'INGREDIENT_NAME_2',
+            image: 'INGREDIENT_IMAGE_2',
+            introduction: 'INGREDIENT_INTRO_2',
+            ABV: 20,
+            price: 100
+        },
+    ],
+    ingredientItem: null,
+    itemStatus: "loading",
+    listStatus: "loading",
+};
+
+const mockDispatch = jest.fn();
+jest.mock("react-redux", () => ({
+    ...jest.requireActual("react-redux"),
+    useDispatch: () => mockDispatch,
+}));
+
+const renderAddIngredientModal = () => {
+    renderWithProviders(
+        <MemoryRouter>
+            <Routes>
+                <Route path="/" element={<AddIngredientModal isOpen={true} close={ jest.fn() } addedIngredientList={[]} setNewIngrdient={ jest.fn() } />} />
+            </Routes>
+        </MemoryRouter>,
+        {
+            preloadedState: {
+                cocktail: stubCocktailInitialState,
+                comment: stubCommentInitialState,
+                ingredient: stubIngredientInitialState,
+            },
+        }
+    );
+};
+
+describe("<AddIngredientModal />", () => {
+    it("should render LoginModal", async () => {
+        renderAddIngredientModal();
+        await screen.findByText("Close");
+    });
+});
