6ecd1d60573b6c1013c5ff194b3ac1c4d21a2b18
nemga123 <hspark0419@naver.com>
2022-11-21 23:06:08 +0900

    feat: Filter by Theme - Refactor filter state structure - Add theme button in filter component

--- frontend/src/NavBar/NavFilter/NavFilter.tsx
+++ frontend/src/NavBar/NavFilter/NavFilter.tsx
@@ -6,6 +6,16 @@ interface ParamList {
     label: string;
 }
 
+interface FilterType {
+    type: "typeOne"|"typeTwo"|"typeThree";
+    name: string;
+}
+
+interface ThemeList {
+    label: string,
+    filters: FilterType[]
+}
+
 export interface Iprops {
     setUrlParams: Dispatch<SetStateAction<string>>,
     handleSearch: any,
@@ -13,29 +23,64 @@ export interface Iprops {
 }
 
 const NavFilter = (prop: Iprops) => {
-
+    const themeList: ThemeList[] = [
+        {label: "Theme1", filters: [{type: "typeOne", name:"_CL"}, {type:"typeThree", name:"weak"}]}, 
+        {label: "Theme2", filters: [{type: "typeOne", name:"_TP"}, {type:"typeThree", name:"medium"}, {type:"typeTwo", name:"_LONG"}]}
+    ]
     const typeOneList: ParamList[] = [{ name: "_CL", label: "클래식" }, { name: "_TP", label: "트로피컬" }]
     const typeTwoList: ParamList[] = [{ name: "_LONG", label: "롱드링크" }, { name: "_SHORT", label: "숏드링크" }, { name: "_SHOT", label: "샷" }]
     const typeThreeList: ParamList[] = [{ name: "weak", label: "15도 이하" }, { name: "medium", label: "15 ~ 30도" }, { name: "strong", label: "30 ~ 40도" }, { name: "extreme", label: "40도 이상" }]
+    const [typeParam, setTypeParam] = useState({
+        typeOne: "",
+        typeTwo: "",
+        typeThree: "",
+    })
     const [search, setSearch] = useState('')
-    const [typeOneParam, setTypeOneParam] = useState<string>("")
-    const [typeTwoParam, setTypeTwoParam] = useState<string>("")
-    const [typeThreeParam, setTypeThreeParam] = useState<string>("")
-    const url_params = `&filter_type_one=${typeOneParam}&filter_type_two=${typeTwoParam}&filter_type_three=${typeThreeParam}&text=${search}`
+    const url_params = `&filter_type_one=${typeParam.typeOne}&filter_type_two=${typeParam.typeTwo}&filter_type_three=${typeParam.typeThree}&text=${search}`
 
-    const onTypeClick = (param: string, setParam: Dispatch<SetStateAction<string>>, type_name: string, unique = false) => {
-        if (unique) {
-            if (param === type_name)
-                setParam("")
+    const onTypeClick = (param_type: "typeOne"|"typeTwo"|"typeThree", type_name: string) => {
+        console.log(typeParam)
+        if(param_type === "typeOne"){
+            console.log("?")
+            const param = typeParam.typeOne
+            if (!param.includes(type_name)){
+                setTypeParam({...typeParam, typeOne:(param+type_name)})
+            }
             else
-                setParam(type_name)
-        }
-        else {
+                setTypeParam({...typeParam, typeOne:(param.replace(type_name, ""))})
+        }else if(param_type === "typeTwo"){
+            const param = typeParam.typeTwo
             if (!param.includes(type_name))
-                setParam(param + type_name)
+                setTypeParam({...typeParam, typeTwo:(param+type_name)})
             else
-                setParam(param.replace(type_name, ""))
+                setTypeParam({...typeParam, typeTwo:(param.replace(type_name, ""))})
+        }else{
+            const param = typeParam.typeThree
+            if (param === type_name)
+                setTypeParam({...typeParam, typeThree:""})
+            else
+                setTypeParam({...typeParam, typeThree:type_name})
         }
+    }
+
+    const onThemeClick = (filters: ThemeList) => {
+        let typeOne = "";
+        let typeTwo = "";
+        let typeThree = "";
+        for(const filter of filters.filters){
+            if(filter.type === "typeOne"){
+                typeOne += filter.name
+            }else if(filter.type === "typeTwo"){
+                typeTwo += filter.name
+            }else{
+                typeThree = filter.name
+            }
+        }
+        setTypeParam({
+            typeOne: typeOne,
+            typeTwo: typeTwo,
+            typeThree: typeThree,
+        })
     }
 
     useEffect(() => prop.setUrlParams(url_params), [url_params])
@@ -56,6 +101,26 @@ const NavFilter = (prop: Iprops) => {
         return (
             <div className="navfilter">
                 <div className="navfilter__wrap">
+                    <div className={"navfilter__title"}>Theme</div>
+                    <div className={"navfilter__content"}>
+                        {themeList.map((type) => {
+                            return (
+                                <React.Fragment key={type.label}>
+                                    <label>
+                                        <button 
+                                            key={type.label} 
+                                            name="theme"
+                                            onChange={() => prop.setUrlParams(url_params)} 
+                                            onClick={() => onThemeClick(type)} 
+                                        />
+                                        {type.label} 
+                                    </label>
+                                </React.Fragment>
+                            )
+                        })}
+                    </div>
+                </div>
+                <div className="navfilter__wrap">
                     <div className={"navfilter__title"}>Type 1</div>
                     <div className={"navfilter__content"}>
                         {typeOneList.map((type) => {
@@ -66,9 +131,9 @@ const NavFilter = (prop: Iprops) => {
                                             key={type.name} 
                                             type="checkbox" 
                                             name="type1"
-                                            checked={typeOneParam.includes(type.name)} 
+                                            checked={typeParam.typeOne.includes(type.name)} 
                                             onChange={() => prop.setUrlParams(url_params)} 
-                                            onClick={() => onTypeClick(typeOneParam, setTypeOneParam, type.name)} 
+                                            onClick={() => onTypeClick("typeOne", type.name)} 
                                         />
                                         {type.label} 
                                     </label>
@@ -88,9 +153,9 @@ const NavFilter = (prop: Iprops) => {
                                             key={type.name} 
                                             type="checkbox" 
                                             name="type2"
-                                            checked={typeTwoParam.includes(type.name)} 
+                                            checked={typeParam.typeTwo.includes(type.name)} 
                                             onChange={() => prop.setUrlParams(url_params)} 
-                                            onClick={() => onTypeClick(typeTwoParam, setTypeTwoParam, type.name)} 
+                                            onClick={() => onTypeClick("typeTwo", type.name)} 
                                         />
                                         {type.label} 
                                     </label>
@@ -109,9 +174,9 @@ const NavFilter = (prop: Iprops) => {
                                         <input 
                                             key={type.name} 
                                             type="checkbox" name="type3"
-                                            checked={typeThreeParam.includes(type.name)} 
+                                            checked={typeParam.typeThree.includes(type.name)} 
                                             onChange={() => prop.setUrlParams(url_params)} 
-                                            onClick={() => onTypeClick(typeThreeParam, setTypeThreeParam, type.name, true)} 
+                                            onClick={() => onTypeClick("typeThree", type.name)} 
                                         />
                                         {type.label} 
                                     </label>
