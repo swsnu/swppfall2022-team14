9a889b8e8b3862b5559e4895f494139cc6f65e66
BECATRUE <becatrueyjh@gmail.com>
2022-11-27 08:39:39 +0900

    refactor: Update rate serializer
    See also: #101

--- backend/qualla/rate/serializers.py
+++ backend/qualla/rate/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from .models import Rate
+from cocktail.serializers import CocktailListSerializer
 
 
 class RateSerializer(serializers.ModelSerializer):
+    cocktail = CocktailListSerializer(read_only=True)
+
     class Meta:
         model = Rate
         fields = (
