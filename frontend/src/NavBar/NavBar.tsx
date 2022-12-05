e61229493a501bdec8932a1423aad45fafa62e1d
BECATRUE <becatrueyjh@gmail.com>
2022-12-05 14:57:14 +0900

    refactor: Add menu icon
    See also: #193

--- frontend/src/NavBar/NavBar.tsx
+++ frontend/src/NavBar/NavBar.tsx
@@ -9,7 +9,9 @@ import LoginModal from "../InitPage/Modals/LoginModal";
 import AddIngredientModal from "../common/Modals/AddIngredientModal";
 import { styled } from '@mui/material/styles';
 import { Box, ListItemIcon, ListItemButton, ListItemText, Stack, IconButton } from '@mui/material';
-import AbcIcon from '@mui/icons-material/Abc';
+import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import KitchenIcon from '@mui/icons-material/Kitchen';
 import CottageIcon from '@mui/icons-material/Cottage';
 import FileUploadIcon from '@mui/icons-material/FileUpload';
 import LiquorIcon from '@mui/icons-material/Liquor';
@@ -132,9 +134,9 @@ const NavBar = () => {
                 })}
             </Stack>
             {[
-                { title: "Standard"  , type: 'ST', onClick: handleST, icon: <AbcIcon /> },
-                { title: "Custom"    , type: 'CS', onClick: handleCS, icon: <AbcIcon /> },
-                { title: "Ingredient", type: 'IG', onClick: handleIG, icon: <AbcIcon /> },
+                { title: "Standard"  , type: 'ST', onClick: handleST, icon: <AutoAwesomeIcon /> },
+                { title: "Custom"    , type: 'CS', onClick: handleCS, icon: <PeopleAltIcon /> },
+                { title: "Ingredient", type: 'IG', onClick: handleIG, icon: <KitchenIcon /> },
             ].map((menu) => {
                 return (
                     <Stack key={menu.title} spacing={0.5}>
