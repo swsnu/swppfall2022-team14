fc278337a43cdb807dc33e019e1988297bcecfb0
AlongwithKiman <expfunsce@snu.ac.kr>
2022-12-13 23:30:36 +0900

    feat: show filter params

--- frontend/src/ListPage/ListPage.tsx
+++ frontend/src/ListPage/ListPage.tsx
@@ -14,8 +14,51 @@ import {
 import { fetchIngredientList, fetchMyIngredientList, IngredientType, selectIngredient } from "../store/slices/ingredient/ingredient";
 import IngredientItem from "../common/Components/IngredientItem";
 import { selectUser } from '../store/slices/user/user';
-import { Grid, Container, Typography, Stack } from "@mui/material";
+import { Grid, Container, Typography, Stack, Box } from "@mui/material";
+import LocalBarIcon from '@mui/icons-material/LocalBar';
 
+function filterParamsToSentence(filterParam: FilterParamType | null) {
+
+    if (!filterParam)
+        return ""
+
+    const { type_one, type_two, type_three } = filterParam;
+
+
+
+    let sentence = ""
+    if (type_one.length === 2)
+        sentence += "클래식한 혹은 여름 느낌의"
+    else if (type_one.length === 1)
+        if (type_one[0] === "클래식")
+            sentence += "클래식한"
+        else if (type_one[0] === "트로피컬")
+            sentence += "여름 느낌의"
+
+
+
+    if (type_three.length > 0 && sentence !== "")
+        sentence += ", "
+    else
+        sentence += " "
+
+    if (type_three[0] === "weak")
+        sentence += "가볍게 마시는 "
+    else if (type_three[0] === "medium")
+        sentence += "은은하게 취하는 "
+    else if (type_three[0] === "strong")
+        sentence += "도수 높은 "
+    else if (type_three[0] == "extreme")
+        sentence += "주당을 위한 "
+
+
+    if (sentence.length === 1)
+        sentence = "모든 칵테일들 목록"
+    else
+        sentence += "칵테일들"
+
+    return sentence
+}
 
 const ListPage = () => {
 
@@ -26,8 +69,8 @@ const ListPage = () => {
     const userState = useSelector(selectUser)
     const [list, setList] = useState<CocktailItemType[]>([])
     const [ingrList, setIngrList] = useState<IngredientType[]>([])
-    const [isLoginOpen, setIsLoginOpen] = useState(false);
     const location = useLocation()
+    const [filterParam, setFilterParam] = useState<FilterParamType | null>(null)
 
     const pageStatus = type === 'ingredient' ? ingrState.listStatus : cocktailState.listStatus
 
@@ -41,13 +84,13 @@ const ListPage = () => {
             available_only: location.state.filter_param.available_only,
             color: location.state.filter_param.color
         }
-
+        setFilterParam(param)
 
 
         if (type === 'standard')
-            dispatch(fetchStandardCocktailList({params:param, token:userState.token}))
+            dispatch(fetchStandardCocktailList({ params: param, token: userState.token }))
         else if (type === 'custom')
-            dispatch(fetchCustomCocktailList({params:param, token:userState.token}))
+            dispatch(fetchCustomCocktailList({ params: param, token: userState.token }))
         else if (type === 'ingredient')
             dispatch(fetchIngredientList(null))
 
@@ -82,8 +125,9 @@ const ListPage = () => {
         <>
             {/*<NavBar />*/}
             <Container sx={{ py: 3 }} >
-                <Typography variant="h4" sx={{ mb: 3 }}>
-                    search filters
+                <Typography variant="h4" sx={{ mb: 3 }} fontFamily="Hi Melody" color="#BC953B" align="center">
+                    {(type !== "ingredient" && cocktailState.cocktailList.length === 0) ? "No Cocktails" : filterParamsToSentence(filterParam)}
+                    {filterParam?.color ? <LocalBarIcon sx={{ fontSize: 40, color: filterParam?.color, ml: 2, }} /> : null}
                 </Typography>
                 {type === 'ingredient' ?
                     <Grid container spacing={3} columns={4}>
