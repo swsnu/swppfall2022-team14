70b70ce0340c2f6ba35b56b7ec7906f42a4e59a2
AlongwithKiman <expfunsce@snu.ac.kr>
2023-09-15 01:02:56 +0900

    f

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -11,7 +11,7 @@ import axios from 'axios';
 
 ReactModal.setAppElement('#root');
 // axios.defaults.baseURL = 'https://qualla.kro.kr/';
-axios.defaults.baseURL = "http://ec2-13-125-227-250.ap-northeast-2.compute.amazonaws.com/"
+axios.defaults.baseURL = "https://tipsybackend.shop/"
 //axios.defaults.baseURL = 'http://localhost:8000/';
 
 const root = ReactDOM.createRoot(
