422d059deaa1a63224166c2781a13442daece0eb
AlongwithKiman <expfunsce@snu.ac.kr>
2023-09-15 00:06:55 +0900

    f

--- backend/qualla/qualla/settings/production.py
+++ backend/qualla/qualla/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['qualla.kro.kr', '13.125.144.254', 'ec2-13-125-144-254.ap-northeast-2.compute.amazonaws.com','15.164.224.81','ec2-15-164-224-81.ap-northeast-2.compute.amazonaws.com']
+ALLOWED_HOSTS = ['tipsybackend.shop','qualla.kro.kr', '13.125.144.254', 'ec2-13-125-144-254.ap-northeast-2.compute.amazonaws.com','15.164.224.81','ec2-15-164-224-81.ap-northeast-2.compute.amazonaws.com']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent.parent
