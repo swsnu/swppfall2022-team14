a3dd26d0b8acab2851540ff71ec481b41c07d854
AlongwithKiman <expfunsce@snu.ac.kr>
2023-09-14 22:10:11 +0900

    f

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -14,7 +14,6 @@ ReactModal.setAppElement('#root');
 axios.defaults.baseURL = "http://ec2-13-125-227-250.ap-northeast-2.compute.amazonaws.com/"
 //axios.defaults.baseURL = 'http://localhost:8000/';
 
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
