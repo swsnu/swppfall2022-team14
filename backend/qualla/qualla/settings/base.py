cdf0349360bc2425d97c2a8f0631045004247c9b
AlongwithKiman <expfunsce@snu.ac.kr>
2023-09-17 17:46:46 +0900

    f

--- backend/qualla/qualla/settings/base.py
+++ backend/qualla/qualla/settings/base.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CORS_ALLOWED_ORIGINS = [
+    "https://tipsynight.site",
     "http://qualla.site.s3-website-ap-northeast-1.amazonaws.com",
     "http://qualla.site.s3-website-ap-n",
     "https://qualla.site",
